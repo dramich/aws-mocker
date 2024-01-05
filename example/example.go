b93c22772e0301f89a23fd82046815c271aa5765
Dan Ramich <dramich@users.noreply.github.com>
2024-01-05 16:26:18 -0700

    Add example file

--- example/example.go
+++ example/example.go
@@ -1,1 +1,38 @@
 package example
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+func main() {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Using the Config value, create the DynamoDB client
+	svc := dynamodb.NewFromConfig(cfg)
+
+	// Build the request with its input parameters
+	svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	svc.BatchGetItem(context.TODO(), nil)
+
+	sts := sts.NewFromConfig(cfg)
+
+	sts.AssumeRole(context.TODO(), nil)
+
+}
